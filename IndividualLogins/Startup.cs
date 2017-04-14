45bed6dc02b2234621b37025490e3b962e7400b7
e2rd\Edvard <e2aard@gmail.com>
2017-04-14 16:30:35 +0300

    Add project files.

---
+++ IndividualLogins/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IndividualLogins.Startup))]
+namespace IndividualLogins
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
